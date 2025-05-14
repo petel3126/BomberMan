36825433014c89ab6c6a5a2322e6c83c26dc7146
Nguyen Van Lam <pthanhlam2016@gmail.com>
2025-05-14 09:50:13 +0700

    them menu

---
+++ src/uet/oop/bomberman/Menu/MenuPause.java
@@ -0,0 +1,44 @@
+package uet.oop.bomberman.Menu;
+
+import javafx.scene.Parent;
+import javafx.scene.layout.VBox;
+
+import static uet.oop.bomberman.BombermanGame.*;
+import static uet.oop.bomberman.graphics.Sound.screen;
+
+public class MenuPause extends Parent {
+    public MenuPause() {
+        VBox menu = new VBox(15);
+        menu.setTranslateX(300);
+        menu.setTranslateY(200);
+
+
+        MyButton MenuBt = new MyButton("Back To Menu");
+        MenuBt.setOnMouseClicked(event -> {
+            entities.clear();
+            enemies.clear();
+            stillObjects.clear();
+            screen.stop();
+            root.getChildren().removeAll(pp, View);
+            root.getChildren().removeAll(bg, pa);
+            root.getChildren().addAll(imageView, r);
+            running = true;
+        });
+
+        MyButton ResumeBt = new MyButton("Resume");
+        ResumeBt.setOnMouseClicked(event -> {
+            isPause = false;
+            running = true;
+            root.getChildren().removeAll(pp, View);
+        });
+        MyButton ExitBt = new MyButton("Exit");
+        ExitBt.setOnMouseClicked(event -> {
+            System.exit(0);
+        });
+
+        menu.getChildren().addAll(MenuBt, ResumeBt, ExitBt);
+
+        getChildren().addAll(menu);
+    }
+}
+
